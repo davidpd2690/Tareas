1331f6b35dd2d6755fc32ecdbbc6cb3ee5bfea83
David Pérez Daher <david.pd2690@gmail.com>
2017-09-08 16:25:33 -0500

    changed code comments to english

--- fibonacci.asm
+++ fibonacci.asm
@@ -14,14 +14,14 @@ main:
 			
 fibonacci:
 
-	addi $sp, $sp, -12	# aparta espacio en stack
-	sw $ra, 0($sp)		# guarda $ra en stack
-	sw $a0, 4($sp)		# guarda $a0 en stack
-	sw $s0, 8($sp)		# guarda fib(n - 1) en stack
+	addi $sp, $sp, -12	# reserve space in stack for 3 regs
+	sw $ra, 0($sp)		# store $ra	in stack
+	sw $a0, 4($sp)		# store $a0	in stack
+	sw $s0, 8($sp)		# store $s0	in stack
 	
 	blt $s0, 2, one		#if (n<2) jump to one
 	
-	addi $s0, $s0, -1	# n - 1
+	addi $s0, $s0, -1	# n - 1	
 	
 	addi $t0,$s0, -2	# n - 2
 	jal fibonacci
@@ -29,10 +29,10 @@ fibonacci:
 	
 	add $v0, $t0, $s0	# fib(n-1) + fib(n-2)
 	
-	lw $ra, 0($sp)		# recupera $ra
-	lw $a0, 4($sp)		# recupera valor de a0 guardado en stack
+	lw $ra, 0($sp)		# load $ra from stack
+	lw $a0, 4($sp)		# load a0 from stack
 	lw $t0, 8($sp)		# 
-	add $sp, $sp, 12	# regresa espacio al stack
+	add $sp, $sp, 12	# restore space in stack
 	jr $ra
 	
 	j exit
